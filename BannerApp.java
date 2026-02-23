f2d8340ee42152d3e95f5258c0fe34562979cd2f
vartikavid-07 <your.email@example.com>
2026-02-23 21:05:19 +0530

    coded uc2 to print '*' and ' '

--- BannerApp.java
+++ BannerApp.java
@@ -1,6 +1,26 @@
 public class BannerApp {
-    public static void main(String[] args){
-        System.out.println("OOPS");
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println("  *****   " + "  *****   " + "  ******  " + "  ******  ");
+
+        // Line 2
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+
+        // Line 3
+        System.out.println("*       * " + "*       * " + "*       * " + "*        ");
+
+        // Line 4
+        System.out.println("*       * " + "*       * " + " ******   " + "  *****   ");
+
+        // Line 5
+        System.out.println("*       * " + "*       * " + "*         " + "       *  ");
+
+        // Line 6
+        System.out.println(" *     *  " + " *     *  " + "*         " + " *     *  ");
+
+        // Line 7
+        System.out.println("  *****   " + "  *****   " + "*         " + "  *****   ");
     }
-    
 }
